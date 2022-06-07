3d9eb0eed4a987fbeb3887543a971761a58ff7a7
daylilyk <daylilykiousis@gmail.com>
2022-06-07 15:16:13 -0700

    JS

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert("Happy Pride!");
+var A= "This month we celebrate the great contributions made by the LGBTQ+ community while recognizing the on going fight they endure to exist daily."
++ "They\'re important members in all communities.";
+document.write(A);
+var B=" Let\'s go gay rights" + ", let\'s go! ";
+document.write(B);
+// Here I am listing out a few letters to define their meaning 
+var LGBTQ = "The beginning of the gay community", L = "Lesbian", G = "Gay", Bi = "Bisexual", T = "Transgender/sexual", Q = "Queer/Questioning";
+document.write(L + G + Bi + T + Q);
---
+++ Basic JavaScript Projects/Project1_expressions_alert/JavaScript_File.js
@@ -0,0 +1,1 @@
+alert("Happy Pride Month!")
