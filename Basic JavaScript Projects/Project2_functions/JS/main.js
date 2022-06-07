ab0fac6fbe990f4a02113e78de794fe68ac1bc70
daylilyk <daylilykiousis@gmail.com>
2022-06-07 15:53:26 -0700

    Button

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+var A= "I dare you to press the button"
+document.write(A)
+function silly_little_function() {
+    var str = "So brave!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
