15e3486f0f0322e451faf3ab7ff076609cf3d7ba
daylilyk <daylilykiousis@gmail.com>
2022-06-08 13:21:57 -0700

    concatenate

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,6 +1,7 @@
 var A= "I dare you to press the button"
 document.write(A)
 function silly_little_function() {
-    var str = "So brave!";
-    document.getElementById("Button_Text").innerHTML = str;
+    var sentence = "So brave";
+    sentence += " of you!";
+    document.getElementById("concatenate").innerHTML = sentence;
 }
