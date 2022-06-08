0269d09bc3eb7b98bd6bcc014cfd6520edc98d92
daylilyk <daylilykiousis@gmail.com>
2022-06-08 13:23:54 -0700

    comments

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,5 +1,7 @@
+//text before button
 var A= "I dare you to press the button"
 document.write(A)
+// Button text after user clicks
 function silly_little_function() {
     var sentence = "So brave";
     sentence += " of you!";
