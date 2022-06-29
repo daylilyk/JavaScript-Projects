40c20909c90ab11d9077c06c67b4f84517b4eef1
daylilyk <daylilykiousis@gmail.com>
2022-06-28 18:29:02 -0700

    Finish

--- JavaScript Projects/Javascript/JavaScript_1.js
+++ JavaScript Projects/Javascript/JavaScript_1.js
@@ -1,7 +1,7 @@
 function Fruit_function() {
     var Fruit_output;
     var Fruits = document.getElementById("Fruit_input").value;
-    var Fruit_string = " is a fine fruit what do you mean! All jokes, we all have out quirks. :3";
+    var Fruit_string = "?? What do you mean? Wrong choice! All jokes, we all have out quirks. :3";
     switch(Fruits) {
         case "Grapes":
             Fruit_output = "Grapes" + Fruit_string;
@@ -24,3 +24,27 @@ function Fruit_function() {
     document.getElementById("Output").innerHTML = Fruit_output;
 
 }
+
+function Fruit_change() {
+    var A = document.getElementsByClassName("Click");
+    A[0].innerHTML = "What is your favorite fruit?";
+}
+
+function Fruit_change1() {
+    var B = document.getElementsByClassName("Click1");
+    B[0].innerHTML = "Pick your favorite fruit from this list:";
+}
+
+var c = document.getElementById("Canvas_ex");
+var ctx = c.getContext("2d");
+ctx.font = "18px Arial";
+ctx.strokeText("Thank you!", 10, 30);
+
+var d = document.getElementById("Canvas_ex2");
+var dtx= d.getContext("2d");
+var grd = dtx.createLinearGradient(0,0,75,0);
+grd.addColorStop(0, "black");
+grd.addColorStop(1, "white");
+
+dtx.fillStyle = grd;
+dtx.fillRect(10,10,70,50);
