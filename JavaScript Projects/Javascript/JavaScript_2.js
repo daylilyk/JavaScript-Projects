ac481c4d7c0e2f66fc329451ba1ccf1cb4722eb5
daylilyk <daylilykiousis@gmail.com>
2022-06-29 14:21:34 -0700

    Java Save

--- JavaScript Projects/Javascript/JavaScript_2.js
+++ JavaScript Projects/Javascript/JavaScript_2.js
@@ -7,6 +7,7 @@ function validateForm() {
     else {
         alert("Thank you for your submission");
     }
+    //pron does not work, need to figure out 
     let y = document.forms["Contact_form"]["Pron"].value;
     if (y=="") {
         alert("Pronouns must be filled out");
