f9289a958bfdb5888bb0dac74c8fe603595ec734
daylilyk <daylilykiousis@gmail.com>
2022-06-29 14:39:18 -0700

    Correcting syntax errors

--- JavaScript Projects/Javascript/JavaScript_3.js
+++ JavaScript Projects/Javascript/JavaScript_3.js
@@ -1,4 +1,4 @@
 function displayType(music) {
-    var musicgenre= music.getAttribute("data-music-genre");
-    alert(musicGenre + " is a great artist in the " + music.innerHTML + " genre!");
+    var musicType= music.getAttribute("data-music-type");
+    alert(musicType + " is a great artist in the " + music.innerHTML + " genre!");
 }
