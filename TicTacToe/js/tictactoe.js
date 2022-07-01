cabf049b67bbe4c115c8db7c77c41b50f72303fe
daylilyk <daylilykiousis@gmail.com>
2022-06-30 17:46:35 -0700

    syntax errors

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -10,7 +10,7 @@ function placeXOrO(squareNumber) {
         if (activePlayer === 'X') {
             select.style.backgroundImage = 'url("images/o.png")';
         } else {
-            select.style.backgroundImage = 'url(images/x.png")';
+            select.style.backgroundImage = 'url("images/x.png")';
         }
 
         selectedSquares.push(squareNumber + activePlayer);
@@ -28,42 +28,6 @@ function placeXOrO(squareNumber) {
         return true;
     }
 
-    function checkWinConditions() {
-        if (arrayIncludes('0X', '1X', '2X')) {drawWinLine(50,100,558,100) }
-        else if (arrayIncludes ('3X', '4X', '5X')) { drawWinLine(50,304,558,304) }
-        else if (arrayIncludes ('6X', '6X', '8X')) { drawWinLine(50,508,558,508) }
-        else if (arrayIncludes('0X', '3X', '6X')) { drawWinLine(100,50,100,558) }
-        else if (arrayIncludes('1X', '4X', '7X')) { drawWinLine(304,50,304,558) }
-        else if (arrayIncludes('2X', '5X', '8X')) { drawWinLine(508,50,508,558) }
-        else if (arrayIncludes('6X', '4X', '2X')) { drawWinLine(100,508,510,90) }
-        else if (arrayIncludes('0X', '4X', '8X')) { drawWinLine(100,100,520,520) }
-        //O array
-        else if (arrayIncludes ('3O', '4O', '5O')) { drawWinLine(50,304,558,304) }
-        else if (arrayIncludes ('6O', '6O', '8O')) { drawWinLine(50,508,558,508) }
-        else if (arrayIncludes('0O', '3O', '6O')) { drawWinLine(100,50,100,558) }
-        else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine(304,50,304,558) }
-        else if (arrayIncludes('2O', '5O', '8O')) { drawWinLine(508,50,508,558) }
-        else if (arrayIncludes('6O', '4O', '2O')) { drawWinLine(100,508,510,90) }
-        else if (arrayIncludes('0O', '4O', '8O')) { drawWinLine(100,100,520,520) }
-        //checking for a tie if none of above conditions are met.
-        //9 squares are selected the code executes.
-        else if (selectedSquares.length >= 9){
-        audio('media/tie.mp3');
-        //sets a .3 second timer before the reset game is called.
-        setTimeout(function () {resetGame(); }, 500);
-        }
-
-        //function checks if an array includes 3 strings, used to check for each win condition.
-        function arrayIncludes(squareA, squareB, squareC) {
-            //these 3 variables will be used to check for 3 in a row.
-            const a = selectedSquares.includes(squareA);
-            const b = selectedSquares.includes(squareB);
-            const c = selectedSquares.includes(squareC);
-            //if the 3 variables we pass are all included in the array then
-            //true is returned else if condition executes frawLine() function.
-            if (a === true && b === true && c === true) {return true; }
-        }
-    }
     function computersTurn() {
         let success = false;
         let pickASquare;
@@ -75,6 +39,43 @@ function placeXOrO(squareNumber) {
             };
         }
     }
+}
+
+function checkWinConditions() {
+    if (arrayIncludes('0X', '1X', '2X')) {drawWinLine(50,100,558,100) }
+    else if (arrayIncludes ('3X', '4X', '5X')) { drawWinLine(50,304,558,304) }
+    else if (arrayIncludes ('6X', '7X', '8X')) { drawWinLine(50,508,558,508) }
+    else if (arrayIncludes('0X', '3X', '6X')) { drawWinLine(100,50,100,558) }
+    else if (arrayIncludes('1X', '4X', '7X')) { drawWinLine(304,50,304,558) }
+    else if (arrayIncludes('2X', '5X', '8X')) { drawWinLine(508,50,508,558) }
+    else if (arrayIncludes('6X', '4X', '2X')) { drawWinLine(100,508,510,90) }
+    else if (arrayIncludes('0X', '4X', '8X')) { drawWinLine(100,100,520,520) }
+    //O array
+    else if (arrayIncludes ('3O', '4O', '5O')) { drawWinLine(50,304,558,304) }
+    else if (arrayIncludes ('6O', '7O', '8O')) { drawWinLine(50,508,558,508) }
+    else if (arrayIncludes('0O', '3O', '6O')) { drawWinLine(100,50,100,558) }
+    else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine(304,50,304,558) }
+    else if (arrayIncludes('2O', '5O', '8O')) { drawWinLine(508,50,508,558) }
+    else if (arrayIncludes('6O', '4O', '2O')) { drawWinLine(100,508,510,90) }
+    else if (arrayIncludes('0O', '4O', '8O')) { drawWinLine(100,100,520,520) }
+    //checking for a tie if none of above conditions are met.
+    //9 squares are selected the code executes.
+    else if (selectedSquares.length >= 9){
+    audio('media/tie.mp3');
+    //sets a .3 second timer before the reset game is called.
+    setTimeout(function () { resetGame(); }, 500);
+    }
+    
+        //function checks if an array includes 3 strings, used to check for each win condition.
+        function arrayIncludes(squareA, squareB, squareC) {
+            //these 3 variables will be used to check for 3 in a row.
+            const a = selectedSquares.includes(squareA);
+            const b = selectedSquares.includes(squareB);
+            const c = selectedSquares.includes(squareC);
+            //if the 3 variables we pass are all included in the array then
+            //true is returned else if condition executes drawLine() function.
+            if (a === true && b === true && c === true) { return true; }
+        }
 }
 
 //this makes the body element temporarily unclickable
@@ -104,7 +105,7 @@ function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
         //variable stores temp x axis data updated in the animation loop
         x = x1,
         //same as above but temp stores y axis data
-        y= y1;
+        y = y1;
 
         function animateLineDrawing() {
             //this variable creats a loop
@@ -128,12 +129,12 @@ function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
                 if (x < x2) { x+= 10; }
                 if (y < y2) { y += 10;}
                 //this condition is similar to those above and is neccessary for 6, 4, 2 win condition.
-                if (x >= x2 && y <= y2) { cancelAnimationFrame(animationLoop); }
+                if (x >= x2 && y >= y2) { cancelAnimationFrame(animationLoop); }
             }
             //this conditinn is similar to the one above. Neccessary for 6, 4, 2 win condition.
-            if (x <= x2 && y1 >= y2) {
+            if (x1 <= x2 && y1 >= y2) {
                 if (x < x2) { x += 10; }
-                if (y > y2) { y -+ 10; }
+                if (y > y2) { y -= 10; }
                 if (x >= x2 && y <= y2) { cancelAnimationFrame(animationLoop); }
             }
         }
